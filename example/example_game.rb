5f970c81bf38805b2bf065525184d4414c3a2d3c
triznylaras <triznylaras@gmail.com>
2022-01-04 16:34:03 +0700

    add function to arrange sudoku board display

--- example/example_game.rb
+++ example/example_game.rb
@@ -1,8 +1,16 @@
 require_relative '../lib/sudoku'
+require 'curses'
 
 sudoku_gen = Sudoku::SudokuGen.new
-puts 'Welcome to Sudoku Game!'
 output = sudoku_gen.grid.each_slice(9).to_a
+
 output.each do |row|
-  puts row.map! { |val| val.zero? ? '_' : val }.join(' ')
+  puts '+-------+-------+-------+'
+  row.map! { |val| val.zero? ? '.' : val }.join(' ')
+  row.each_slice(3) do |k, l, m|
+    new_arr = "| #{k} #{l} #{m} "
+    print new_arr
+  end
+  print '|'
+  puts
 end
